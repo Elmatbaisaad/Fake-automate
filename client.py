b3990022eb6c1996e6078606bf94ae3c0f9c9dea
elmatbaisaad <elmatbaisaad@gmail.com>
2022-04-11 10:02:16 +0200

    modification client fake automate

--- client.py
+++ client.py
@@ -4,17 +4,24 @@ import json
 with open('configuration.json','r') as f:
     data = json.load(f)
 
+def adresse_register(x):
+    return data["mot"][x]["adresse"]
+def adresse_coil(x):
+    return data["bit"][x]["adresse"]
+
+
+
+
 adresse_ip = data["serveur"][0]["adresse"]
 port = data["serveur"][0]["port"]
 
 client = ModbusClient(adresse_ip,port)
 client.open()
 
-regs_l=client.read_holding_registers(data["mot"][0]["adresse"])
+regs_l=client.read_holding_registers(adresse_register(0))
 print('register  : %s' % regs_l)
 
 
 
-
-regs_2=client.read_coils(data["bit"][1]["adresse"])
+regs_2=client.read_coils(adresse_coil(1))
 print('coil : %s'% regs_2)
